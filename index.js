17719deb1164952e2f02af5d1f935de031cc592f
Abishek <66282693+TentacioPro@users.noreply.github.com>
2024-03-18 15:15:57 +0530

    update Backend v2.0

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import { MongoClient } from 'mongodb';
 import { ObjectId } from "mongodb";
 import cors from "cors";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 const app = express();
@@ -13,49 +14,95 @@ console.log("mongoDB connected successfully");
 
 app.use(express.json());
 app.use(cors());
+const auth = (request, response, next) => {
+    try {
+        const token = request.header("backend-token"); // keyname
+        jwt.verify(token, "student");
+        next();
+
+    } catch (error) {
+        response.status(401).send({message:error.message});        
+    }
+};
 
 app.get("/",function(request,response){
-    response.send("hello world")
+    response.status(200).send("hello world")
 });
 
 app.post("/post",async function(request,response){
     const getPostman = request.body;
     const sendMethod = await client.db("CRUD").collection("data").insertOne(getPostman);
-    response.send(sendMethod);
+    response.status(201).send(sendMethod);
 
 });
 app.post("/postmany", async function(request,response){
     const getMany = request.body;
     const sendMethod = await client.db("CRUD").collection("data").insertMany(getMany);
-    response.send(sendMethod);
+    response.status(201).send(sendMethod);
 });
 
-app.get("/get",async function(request,response){
+app.get("/get",auth,async function(request,response){
     const getMethod = await client.db("CRUD").collection("data").find({}).toArray();
-    response.send(getMethod);
+    response.status(200).send(getMethod);
 });
 
 app.get("/getone/:id",async function(request,response){
     const {id} = request.params ;
     const getMethod = await client.db("CRUD").collection("data").findOne({_id:new ObjectId(id)});
-    response.send(getMethod);
+    response.status(200).send(getMethod);
 });
 
 app.put("/update/:id",async function(request,response){
     const {id} = request.params;
     const getPostman = request.body;
     const updateMethod = await client.db("CRUD").collection("data").updateOne({_id:new ObjectId(id)}, {$set:getPostman});
-    response.send(updateMethod);
+    response.status(201).send(updateMethod);
     
 });
 
 app.delete("/delete/:id",async function(request,response){
     const {id} = request.params;
     const deleteMethod = await client.db("CRUD").collection("data").deleteOne({_id:new ObjectId(id)});
-    response.send(deleteMethod);
+    response.status(200).send(deleteMethod);
 });
 
+app.post("/register", async function(request,response){
+    const {username, email, password} = request.body;
+    const userFind = await client.db("CRUD").collection("private").findOne({email:email});
+    if(userFind){
+        response.status(400).send("Existing User");
+    }
+    else{
+         
+         const salt = await bcrypt.genSalt(10);
+         const hashedPassword = await bcrypt.hash(password,salt);
+         console.log(hashedPassword);
+         const registerMethod = await client.db("CRUD").collection("private").insertOne({username:username, email:email, password: hashedPassword});
+         response.status(201).send(registerMethod); //201 - creation 200 - ok 400 - bad req
+    }    
+})
 
+app.post("/login",async function(request,response){
+    const {email, password} = request.body;
+    // console.log(email,password);
+    const userFind = await client.db("CRUD").collection("private").findOne({email:email});
+    // console.log(userFind);
+    if(userFind) {
+        const mongoDBpassword = userFind.password;
+        const passwordCheck = await bcrypt.compare(password, mongoDBpassword);
+        // console.log(passwordCheck);
+        if(passwordCheck) {
+            const token = jwt.sign({id:userFind._id} , "student"); //jwt token student
+            response.status(200).send({token:token});
+        }
+        else{
+            response.status(400).send("Invalid Password");
+        }
+    }
+    else {
+        response.status(400).send("Invalid email-id");
+    }
+});
 
 app.listen(4000, () => {
     console.log("server connected successfully");
